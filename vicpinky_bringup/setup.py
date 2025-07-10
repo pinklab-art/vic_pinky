15ec211b595ef0d573cdf0669eef2d00035150ca
mkh <kyung133851@pinklab.art>
2025-07-10 17:10:10 +0900

    modify bringup setup.py

--- vicpinky_bringup/setup.py
+++ vicpinky_bringup/setup.py
@@ -23,7 +23,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            "vicpinky_bringup=vicpinky_bringup.vicpinky_bringup:main"
+            "bringup=vicpinky_bringup.bringup:main"
         ],
     },
 )
